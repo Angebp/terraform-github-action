df7ff1a0183429f52a435f9f99ec5c92c94c1f94
angebp <angelo_b_pinheiro@outlook.com>
2022-08-12 13:53:58 -0300

    Ajuste azure provider

---
+++ src/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "locat" {
+ default = "EastUS"
+}
+
+variable "rg" {
+default = "RG_BPINFRADEV_DEV"
+  
+}
+ variable "vnet"{
+    default = "vnet-bpinfra-iac"
+ } 
+
+variable "vm" {
+   default = "vmtiac"
+  
+}
